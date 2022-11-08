6d4be5ac3e91f909ef1ab507a140fc30b12660c5
jquinzell <>
2022-11-07 20:04:04 -0600

    display songs in playlist

--- src/components/PlaylistSidebar.tsx
+++ src/components/PlaylistSidebar.tsx
@@ -1,4 +1,4 @@
-import { Playlist } from "../utils/trpc";
+import { Playlist, trpc } from "../utils/trpc";
 
 interface Props {
   className?: string;
@@ -11,13 +11,68 @@ export const PlaylistSidebar: React.FC<Props> = ({
   onClickChangePlaylist,
   playlist,
 }) => {
+  console.log(playlist);
+  const playlistId = playlist?.id ?? "";
+  const { data } = trpc.spotify.getPlaylistTracks.useQuery(playlistId, {
+    enabled: !!playlistId,
+  });
+  console.log(data?.[0]);
   return (
     <div className={`flex h-5/6 w-96 flex-col ${className} mr-4`}>
       <h3 className="m-4 text-2xl">{playlist?.name ?? "Playlist"}</h3>
-      <div className="relative flex grow flex-col place-items-center justify-center rounded-lg bg-base-200 p-4">
-        <p className="text-lg">Select a playlist to get started!</p>
+      <div className="relative flex grow flex-col place-items-center justify-center overflow-y-scroll rounded-lg bg-base-200 p-4">
+        {data ? (
+          <div>
+            {data.map(({ track }) => {
+              const image = track.album.images[0];
+              return (
+                <div
+                  key={track.id}
+                  className="group card h-min w-48 place-self-auto rounded-none bg-base-200 shadow-xl hover:bg-base-300"
+                >
+                  {/* TODO: add a placeholder image? */}
+                  <figure className="h-48 w-full p-5">
+                    {image && (
+                      // TODO: how to make next/Image work
+                      // eslint-disable-next-line @next/next/no-img-element
+                      <img
+                        className="h-full w-full"
+                        src={image.url}
+                        alt={track.name}
+                      />
+                    )}
+                  </figure>
+                  <div className="card-body relative flex-initial justify-between p-4">
+                    <h3 className="card-title text-base">{track.name}</h3>
+                    <button
+                      // onClick={() => onSelectPlaylist(playlist)}
+                      className="btn-primary btn-circle btn invisible absolute bottom-3 right-3 group-hover:visible"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <p className="mt-auto text-lg">Select a playlist to get started!</p>
+        )}
         <button
-          className="btn-primary btn-circle btn absolute bottom-4 right-4"
+          className="btn-primary btn-circle btn sticky bottom-0 mt-auto place-self-end"
           onClick={() => onClickChangePlaylist()}
         >
           <svg
--- src/server/trpc/router/spotify.ts
+++ src/server/trpc/router/spotify.ts
@@ -16,28 +16,55 @@ const TokenResponse = z.object({
 });
 
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-playlist
-const PlaylistSchema = z.object({
-  description: z.string().nullable(),
-  external_urls: z.object({ spotify: z.string() }),
-  href: z.string(),
-  id: z.string(),
-  images: z.array(
-    z.object({
-      url: z.string(),
-      height: z.number().nullable(),
-      width: z.number().nullable(),
-    })
-  ),
-  name: z.string(),
-  // tracks: z.object({href:z.string()}),
-  type: z.literal("playlist"),
-  // uri: z.string(),
-});
+const PlaylistSchema = z
+  .object({
+    description: z.string().nullable(),
+    external_urls: z.object({ spotify: z.string() }),
+    href: z.string(),
+    id: z.string(),
+    images: z.array(
+      z.object({
+        url: z.string(),
+        height: z.number().nullable(),
+        width: z.number().nullable(),
+      })
+    ),
+    name: z.string(),
+    // tracks: z.object({href:z.string()}),
+    type: z.literal("playlist"),
+    // uri: z.string(),
+  })
+  // TODO: will probably remove passThrough eventually?
+  .passthrough();
 
 export type Playlist = z.infer<typeof PlaylistSchema>;
 
 const UserPlaylistsResponse = ResourceResponse.extend({
   items: z.array(PlaylistSchema),
+});
+
+const TracksSchema = z
+  .object({
+    track: z
+      .object({
+        id: z.string(),
+        name: z.string(),
+        album: z.object({
+          images: z.array(
+            z.object({
+              url: z.string(),
+              height: z.number().nullable(),
+              width: z.number().nullable(),
+            })
+          ),
+        }),
+      })
+      .passthrough(),
+  })
+  .passthrough();
+
+const PlaylistTracksResponse = ResourceResponse.extend({
+  items: z.array(TracksSchema),
 });
 
 async function refreshToken(refresh_token: string) {
@@ -87,4 +114,27 @@ export const spotifyRouter = router({
     const { access_token } = await refreshToken(account.refresh_token || "");
     return getPlaylists(access_token, account.providerAccountId);
   }),
+  getPlaylistTracks: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const account = await ctx.prisma.account.findFirstOrThrow({
+        where: { userId },
+      });
+      const { access_token: accessToken } = await refreshToken(
+        account.refresh_token || ""
+      );
+      const params = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const spotifyResponse = await fetch(
+        `https://api.spotify.com/v1/playlists/${input}/tracks`,
+        params
+      );
+      const json = await spotifyResponse.json();
+      const data = PlaylistTracksResponse.parse(json);
+      return data.items;
+    }),
 });
