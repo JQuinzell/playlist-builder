b2ae39abe8f777c46f71df909d1c8b3a8eb1aea5
jquinzell <>
2022-11-16 20:46:49 -0600

    use thinner icons

--- src/components/TrackReview.tsx
+++ src/components/TrackReview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Track } from "../utils/trpc";
 import { TrackCard } from "./TrackCard";
-import { HiPlus, HiX } from "react-icons/hi";
+import { HiPlus, HiXMark } from "react-icons/hi2";
 
 interface Props {
   track: Track;
@@ -11,7 +11,7 @@ export const TrackReview: React.FC<Props> = ({ track }) => {
     <TrackCard image={track.images[0]?.url} name={track.name}>
       <div className="flex">
         <button className="btn-circle btn h-20 w-20">
-          <HiX className="h-20 w-20" />
+          <HiXMark className="h-20 w-20" />
         </button>
         <button className="btn-primary btn-circle btn ml-auto h-20 w-20">
           <HiPlus className="h-20 w-20" />
