07eadb56f2cd02db469f21246eab45371e2e8cb7
jquinzell <>
2022-12-04 17:58:10 -0600

    handle adding songs & not adding

--- src/components/SongBar.tsx
+++ src/components/SongBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Source, Track, trpc } from "../utils/trpc";
 import { TrackCard } from "./TrackCard";
 import { TrackReview } from "./TrackReview";
@@ -10,14 +10,28 @@ interface Props {
 
 export const SongBar: React.FC<Props> = ({ sources, playlistId }) => {
   const { data } = trpc.spotify.getSourceTracks.useQuery(sources);
+  const [removedSongs, setRemovedSongs] = useState<
+    Record<string, boolean | undefined>
+  >({});
   const items = data ?? [];
+
+  function removeSong(track: Track) {
+    setRemovedSongs((songs) => ({ ...songs, [track.id]: true }));
+  }
   return (
     <div className="flex h-full flex-col gap-4 overflow-y-scroll">
-      {items.map((track) => (
-        <div key={track.id} className="w-96 shrink-0">
-          <TrackReview track={track} playlistId={playlistId} />
-        </div>
-      ))}
+      {items
+        .filter((track) => !removedSongs[track.id])
+        .map((track) => (
+          <div key={track.id} className="w-96 shrink-0">
+            <TrackReview
+              track={track}
+              playlistId={playlistId}
+              onAdd={() => removeSong(track)}
+              onRemove={() => removeSong(track)}
+            />
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/TrackReview.tsx
+++ src/components/TrackReview.tsx
@@ -6,17 +6,39 @@ import { HiXMark, HiCheck } from "react-icons/hi2";
 interface Props {
   track: Track;
   playlistId: string;
+  onAdd: () => void;
+  onRemove: () => void;
 }
-export const TrackReview: React.FC<Props> = ({ track, playlistId }) => {
-  const mutation = trpc.spotify.addTrackToPlaylist.useMutation();
+export const TrackReview: React.FC<Props> = ({
+  track,
+  playlistId,
+  onAdd,
+  onRemove,
+}) => {
+  const utils = trpc.useContext();
+  const mutation = trpc.spotify.addTrackToPlaylist.useMutation({
+    onSuccess() {
+      utils.spotify.getPlaylistTracks.invalidate();
+    },
+  });
+
+  function onClickAdd() {
+    mutation.mutate({ id: track.id, playlistId });
+    onAdd();
+  }
+
+  function onClickRemove() {
+    onRemove();
+  }
+
   return (
     <TrackCard image={track.images[0]?.url} name={track.name}>
       <div className="flex">
         <button className="btn-circle btn h-20 w-20">
-          <HiXMark className="h-20 w-20" />
+          <HiXMark className="h-20 w-20" onClick={onClickRemove} />
         </button>
         <button
-          onClick={() => mutation.mutate({ id: track.id, playlistId })}
+          onClick={onClickAdd}
           className="btn-primary btn-circle btn ml-auto h-20 w-20"
         >
           <HiCheck className="h-20 w-20" />
